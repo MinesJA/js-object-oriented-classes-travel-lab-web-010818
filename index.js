5035d9b2ac0809f2289adc3f97bbe38befd52bf2
MinesJA <minesja@gmail.com>
2018-02-22 16:20:05 -0500

    Done.

--- index.js
+++ index.js
@@ -17,18 +17,31 @@ class Route{
     this.endingLocation = endingLocation;
   }
 
-  let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-
   blocksTravelled(){
+    let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
     let startStreet = this.beginningLocation.vertical;
     let endStreet = this.endingLocation.vertical;
     let startAve = eastWest.indexOf(this.beginningLocation.horizontal);
-    let endAve = eastWest.indexOf(this.beginningLocation.horizontal);
+    let endAve = eastWest.indexOf(this.endingLocation.horizontal);
 
     let vertDist = Math.abs(startStreet - endStreet);
     let horzDist = Math.abs(startAve - endAve);
 
-    debugger
     return vertDist + horzDist
     }
+
+  estimatedTime(peak = false){
+    // 3 blocks = 1 min
+    // return time in minutes
+    let blocks = this.blocksTravelled();
+
+    if (peak){
+      return blocks/2;
+    } else {
+      return blocks/3;
+    }
+  }
+
+
 }
